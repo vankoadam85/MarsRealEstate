d02ed5abae16601e1588018498a4910c06bccb47
Vankó Ádám <vankoadam85@gmail.com>
2021-04-19 13:01:13 +0200

    add adapter for displaying MarsProperty object

--- app/src/main/java/com/example/android/marsrealestate/overview/PhotoGridAdapter.kt
+++ app/src/main/java/com/example/android/marsrealestate/overview/PhotoGridAdapter.kt
@@ -16,3 +16,49 @@
  */
 
 package com.example.android.marsrealestate.overview
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.android.marsrealestate.databinding.GridViewItemBinding
+import com.example.android.marsrealestate.network.MarsProperty
+
+
+class PhotoGridAdapter : ListAdapter<MarsProperty, PhotoGridAdapter.MarsPropertyViewHolder>(DiffCallback) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MarsPropertyViewHolder {
+        return MarsPropertyViewHolder.from(parent)
+    }
+
+    override fun onBindViewHolder(holder: MarsPropertyViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+
+    companion object DiffCallback : DiffUtil.ItemCallback<MarsProperty>() {
+
+        override fun areItemsTheSame(oldItem: MarsProperty, newItem: MarsProperty) =
+                oldItem === newItem
+
+        override fun areContentsTheSame(oldItem: MarsProperty, newItem: MarsProperty) =
+                oldItem.id == newItem.id
+    }
+
+    class MarsPropertyViewHolder(private val binding: GridViewItemBinding) : RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(property: MarsProperty) {
+            binding.property = property
+            binding.executePendingBindings()
+        }
+
+        companion object {
+            fun from(parent: ViewGroup): MarsPropertyViewHolder {
+                val inflater = LayoutInflater.from(parent.context)
+                val binding = GridViewItemBinding.inflate(inflater)
+                return MarsPropertyViewHolder(binding)
+            }
+        }
+    }
+
+}
