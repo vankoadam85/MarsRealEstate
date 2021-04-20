8507f39a027d88e3ae3487d0b51a59d45e0e42db
Vankó Ádám <vankoadam85@gmail.com>
2021-04-20 14:53:32 +0200

    add instantiation and binding of DetailViewModel

--- app/src/main/java/com/example/android/marsrealestate/detail/DetailFragment.kt
+++ app/src/main/java/com/example/android/marsrealestate/detail/DetailFragment.kt
@@ -31,10 +31,14 @@ class DetailFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
 
-        @Suppress("UNUSED_VARIABLE")
         val application = requireNotNull(activity).application
         val binding = FragmentDetailBinding.inflate(inflater)
         binding.lifecycleOwner = this
+
+        val marsProperty = DetailFragmentArgs.fromBundle(requireArguments()).selectedProperty
+        binding.viewModel = ViewModelProvider(this, DetailViewModelFactory(marsProperty, application))
+                .get(DetailViewModel::class.java)
+
         return binding.root
     }
 }
